af47115efc49123bd91d92b72ee639c7b9fc9b98
Stephen Chen <82287719+DarkSide0918@users.noreply.github.com>
2021-06-25 09:53:38 -0400

    Initial Commit - Pulled from previous repository
    Initial Commit

---
+++ src/scripts/start.js
@@ -0,0 +1,7 @@
+var loggedIn = false;
+var manualLogin = false;
+var autoLogin = false;
+var userEmail = "";
+var userPassword = "";
+var userName = "";
+var myRoomName = "";
---
+++ src/scripts/title.js
@@ -0,0 +1,11 @@
+setTimeout(() => {
+	
+	document.getElementById("title-line-2").hidden = false;
+	
+}, 2250);
+
+setTimeout(() => {
+	
+	document.getElementById("title-line-1").style = "border: none";
+	
+}, 1875);
