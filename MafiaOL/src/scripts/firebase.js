347e0edead88e4840f9e7900cb7f5af9d104bc94
Stephen Chen <82287719+DarkSide0918@users.noreply.github.com>
2021-06-29 11:51:17 -0400

    Synced latest progress

---
+++ MafiaOL/src/scripts/firebase.js
@@ -0,0 +1,19 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+var firebaseConfig = {
+
+	apiKey: "AIzaSyC7b5vUhrAdQwwwi-mHoid-Hp5RhacGNsU",
+	authDomain: "mafiaol.firebaseapp.com",
+	databaseURL: "https://mafiaol-default-rtdb.firebaseio.com",
+	projectId: "mafiaol",
+	storageBucket: "mafiaol.appspot.com",
+	messagingSenderId: "546041985264",
+	appId: "1:546041985264:web:44b3bc0271f089c985aa35",
+	measurementId: "G-XKX9TT03KL"
+
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
---
+++ MafiaOL/src/scripts/scene-manager.js
@@ -0,0 +1,91 @@
+function switchScenes(scene) {
+
+	closeAllScenes();
+
+	switch (scene) {
+
+		case "authentication":
+
+			if (autoLogin) {
+
+				switchScenes("home");
+
+			} else {
+
+				document.getElementById("authentication-scene").hidden = false;
+				document.getElementById("master-navigation-buttons-container").hidden = true;
+
+			}
+
+		break;
+
+		case "room-list": 
+
+			document.getElementById("room-list-scene").hidden = false;
+			document.getElementById("master-navigation-buttons-container").hidden = false;
+
+		break;
+
+		case "home": 
+
+			document.getElementById("home-scene").hidden = false;
+			document.getElementById("master-navigation-buttons-container").hidden = false;
+
+		break;
+
+		case "account":
+
+			loadAccountInformation();
+
+			document.getElementById("account-scene").hidden = false;
+			document.getElementById("master-navigation-buttons-container").hidden = false;
+
+		break;
+
+		case "create-room":
+
+			document.getElementById("create-room-scene").hidden = false;
+			document.getElementById("master-navigation-buttons-container").hidden = false;
+
+		break;
+
+		case "room": 
+
+			document.getElementById("room-scene").hidden = false;
+			document.getElementById("master-navigation-buttons-container").hidden = false;
+
+		break;
+
+		default: 
+
+			document.getElementById("coming-soon-scene").hidden = false;
+			document.getElementById("master-navigation-buttons-container").hidden = false;
+
+		break;
+
+	}
+
+}
+
+function closeAllScenes() {
+
+	// Hide All Scenes
+	document.getElementById("introduction-scene").hidden = true;
+	document.getElementById("authentication-scene").hidden = true;
+	document.getElementById("home-scene").hidden = true;
+	document.getElementById("room-list-scene").hidden = true;
+	document.getElementById("room-scene").hidden = true;
+	document.getElementById("game-scene").hidden = true;
+	document.getElementById("account-scene").hidden = true;
+	document.getElementById("settings-scene").hidden = true;
+	document.getElementById("store-scene").hidden = true;
+	document.getElementById("social-scene").hidden = true;
+	document.getElementById("create-room-scene").hidden = true;
+	document.getElementById("coming-soon-scene").hidden = true;
+
+}
+
+function loading() {
+
+
+}
