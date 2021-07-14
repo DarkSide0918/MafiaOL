7d21b5129ccc5731785defd4bb2928e8e0158a50
Stephen Chen <82287719+DarkSide0918@users.noreply.github.com>
2021-07-14 19:49:24 -0400

    add: host end round functionality

---
+++ src/scripts/host.js
@@ -0,0 +1,62 @@
+function funcEndRound() {
+
+    firebase.database().ref("rooms/" + myRoomName + "/characters").on("value", function (snapshot) {
+
+        var arrCharacterKeys = Object.keys(snapshot.val());
+        var round = 0;
+
+        for (var i = 1; i <= arrCharacterKeys.length; i++) {
+
+            switch (snapshot.child(i).child("status").val()) {
+
+                case "b":
+                case "t":
+
+                    firebase.database().ref("rooms/" + myRoomName + "/characters/" + i).update({
+
+                        status: "n"
+
+                    });
+
+                break;
+
+                case "i":
+                case "sa":
+
+                    firebase.database().ref("rooms/" + myRoomName + "/characters/" + i).update({
+
+                        status: "k"
+
+                    });
+
+                break;
+
+                case "si":
+
+                    firebase.database().ref("rooms/" + myRoomName + "/characters/" + i).update({
+
+                        status: "i"
+
+                    });
+
+                break;
+
+            }
+
+        }
+
+    });
+
+    firebase.database.ref("rooms/" + myRoomName + "round").once("value", function(snapshot) {
+
+        round = snapshot.val();
+
+    });
+
+    firebase.database.ref("rooms/" + myRoomName).update({
+
+        round: round + 1
+
+    });
+
+}
